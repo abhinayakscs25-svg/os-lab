9c2188ddfdd21bc3fc13f47ecddb52ae1f089174
abhinayakscs25-svg <abhinayaks.cs25@bmsce.ac.in>
2026-04-28 12:27:37 +0530

    Add files via upload

---
+++ lab 8/banker's algorithm.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+
+#define MAX 10
+
+int main() {
+    int n, m;
+    int alloc[MAX][MAX], max[MAX][MAX], need[MAX][MAX];
+    int avail[MAX];
+    int finish[MAX] = {0};
+    int safeSeq[MAX];
+
+    printf("Enter number of processes: ");
+    scanf("%d", &n);
+
+    printf("Enter number of resources: ");
+    scanf("%d", &m);
+
+    printf("Enter Allocation Matrix:\n");
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            scanf("%d", &alloc[i][j]);
+        }
+    }
+
+    printf("Enter Maximum Demand Matrix:\n");
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            scanf("%d", &max[i][j]);
+        }
+    }
+
+    printf("Enter Available Resources:\n");
+    for(int i = 0; i < m; i++) {
+        scanf("%d", &avail[i]);
+    }
+
+    // Calculate Need matrix
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            need[i][j] = max[i][j] - alloc[i][j];
+        }
+    }
+
+    int count = 0;
+    while(count < n) {
+        int found = 0;
+        for(int i = 0; i < n; i++) {
+            if(finish[i] == 0) {
+                int j;
+                for(j = 0; j < m; j++) {
+                    if(need[i][j] > avail[j])
+                        break;
+                }
+
+                if(j == m) {
+                    for(int k = 0; k < m; k++) {
+                        avail[k] += alloc[i][k];
+                    }
+                    safeSeq[count++] = i;
+                    finish[i] = 1;
+                    found = 1;
+                }
+            }
+        }
+
+        if(found == 0) {
+            printf("\nSystem is not in a safe state.\n");
+            return 0;
+        }
+    }
+
+    printf("\nSystem is in a safe state.\n");
+    printf("Safe sequence is: ");
+    for(int i = 0; i < n; i++) {
+        printf("P%d", safeSeq[i]);
+        if(i != n - 1)
+            printf(" -> ");
+    }
+    printf("\n");
+
+    return 0;
+}
